5bfd454665e7bedeba839825de64b9937bdce36e
Lessley Dennington <lessleydennington@gmail.com>
2021-10-15 21:20:34 +0000

    diff: enable and test the sparse index
    Enable the sparse index within the 'git diff' command. Its implementation
    already safely integrates with the sparse index because it shares code with
    the 'git status' and 'git checkout' commands that were already integrated.
    The most interesting thing to do is to add tests that verify that 'git diff'
    behaves correctly when the sparse index is enabled. These cases are:
    
    1. The index is not expanded for 'diff' and 'diff --staged'
    2. 'diff' and 'diff --staged' behave the same in full checkout, sparse
    checkout, and sparse index repositories in the following partially-staged
    scenarios (i.e. the index, HEAD, and working directory differ at a given
    path):
        1. Path is within sparse-checkout cone
        2. Path is outside sparse-checkout cone
        3. A merge conflict exists for paths outside sparse-checkout cone
    
    The `p2000` tests demonstrate a ~30% execution time reduction for 'git
    diff' and a ~75% execution time reduction for 'git diff --staged' using a
    sparse index:
    
    Test                                      before  after
    -------------------------------------------------------------
    2000.30: git diff (full-v3)               0.37    0.36 -2.7%
    2000.31: git diff (full-v4)               0.36    0.35 -2.8%
    2000.32: git diff (sparse-v3)             0.46    0.30 -34.8%
    2000.33: git diff (sparse-v4)             0.43    0.31 -27.9%
    2000.34: git diff --staged (full-v3)      0.08    0.08 +0.0%
    2000.35: git diff --staged (full-v4)      0.08    0.08 +0.0%
    2000.36: git diff --staged (sparse-v3)    0.17    0.04 -76.5%
    2000.37: git diff --staged (sparse-v4)    0.16    0.04 -75.0%
    
    Co-authored-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Lessley Dennington <lessleydennington@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1092-sparse-checkout-compatibility.sh
+++ t/t1092-sparse-checkout-compatibility.sh
@@ -416,6 +416,46 @@ test_expect_success 'diff --staged' '
 	test_all_match git diff --staged
 '
 
+test_expect_success 'diff partially-staged' '
+	init_repos &&
+
+	write_script edit-contents <<-\EOF &&
+	echo text >>$1
+	EOF
+
+	# Add file within cone
+	test_sparse_match git sparse-checkout set deep &&
+	run_on_all ../edit-contents deep/testfile &&
+	test_all_match git add deep/testfile &&
+	run_on_all ../edit-contents deep/testfile &&
+
+	test_all_match git diff &&
+	test_all_match git diff --staged &&
+
+	# Add file outside cone
+	test_all_match git reset --hard &&
+	run_on_all mkdir newdirectory &&
+	run_on_all ../edit-contents newdirectory/testfile &&
+	test_sparse_match git sparse-checkout set newdirectory &&
+	test_all_match git add newdirectory/testfile &&
+	run_on_all ../edit-contents newdirectory/testfile &&
+	test_sparse_match git sparse-checkout set &&
+
+	test_all_match git diff &&
+	test_all_match git diff --staged &&
+
+	# Merge conflict outside cone
+	# The sparse checkout will report a warning that is not in the
+	# full checkout, so we use `run_on_all` instead of
+	# `test_all_match`
+	run_on_all git reset --hard &&
+	test_all_match git checkout merge-left &&
+	test_all_match test_must_fail git merge merge-right &&
+
+	test_all_match git diff &&
+	test_all_match git diff --staged
+'
+
 # NEEDSWORK: sparse-checkout behaves differently from full-checkout when
 # running this test with 'df-conflict-2' after 'df-conflict-1'.
 test_expect_success 'diff with renames and conflicts' '
@@ -861,6 +901,11 @@ test_expect_success 'sparse-index is not expanded' '
 
 	# Wildcard identifies only full sparse directories, no index expansion
 	ensure_not_expanded reset deepest -- folder\* &&
+
+	echo a test change >>sparse-index/README.md &&
+	ensure_not_expanded diff &&
+	git -C sparse-index add README.md &&
+	ensure_not_expanded diff --staged &&
 
 	ensure_not_expanded checkout -f update-deep &&
 	test_config -C sparse-index pull.twohead ort &&
